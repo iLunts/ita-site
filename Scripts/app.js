4547f4c5be55a7abd397cb24a84d2a3517d1bce5
Roman Luntsevich <shipik2008@gmail.com>
2017-02-09 18:15:03 +0300

    add text on page vacancies, news and other

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,3 +10,44 @@ app.config(['$locationProvider', '$routeProvider', function ($locationProvider, 
 
     // $routeProvider.otherwise({ redirectTo: "/" });
 }]);
+
+app.directive("owlCarousel", function() {
+    return {
+        restrict: 'E',
+        transclude: false,
+        link: function (scope) {
+            scope.initCarousel = function(element) {
+                var defaultOptions = {};
+                var customOptions = scope.$eval($(element).attr('data-options'));
+                for(var key in customOptions) {
+                    defaultOptions[key] = customOptions[key];
+                }
+                $(element).owlCarousel(defaultOptions);
+            };
+        }
+    };
+})
+.directive('owlCarouselItem', [function() {
+    return {
+        restrict: 'A',
+        transclude: false,
+        link: function(scope, element) {
+
+            if(scope.$last) {
+                scope.initCarousel(element.parent());
+            }
+        }
+    };
+}]);
+
+
+app.controller('sliderCtrl', function($scope){
+
+    $scope.slider = [
+        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
+        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
+        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
+        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'}
+    ];
+
+});
--- view/home/homeCtrl.js
+++ view/home/homeCtrl.js
@@ -1,40 +1,11 @@
-app.directive("owlCarousel", function() {
-    return {
-        restrict: 'E',
-        transclude: false,
-        link: function (scope) {
-            scope.initCarousel = function(element) {
-                var defaultOptions = {};
-                var customOptions = scope.$eval($(element).attr('data-options'));
-                for(var key in customOptions) {
-                    defaultOptions[key] = customOptions[key];
-                }
-                $(element).owlCarousel(defaultOptions);
-            };
-        }
-    };
-})
-.directive('owlCarouselItem', [function() {
-    return {
-        restrict: 'A',
-        transclude: false,
-        link: function(scope, element) {
-
-            if(scope.$last) {
-                scope.initCarousel(element.parent());
-            }
-        }
-    };
-}]);
-
 app.controller('homeCtrl', function($scope){
 
-    $scope.slider = [
-        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
-        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
-        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
-        {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'}
-    ];
+    // $scope.slider = [
+    //     {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
+    //     {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
+    //     {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'},
+    //     {url: '/content/img/slider/estate.png', title: 'ESTATE', subtitle: 'агентства недвижимости', slogan: '- это «новое слово» в работе агентств недвижимости!'}
+    // ];
 
 });
 
